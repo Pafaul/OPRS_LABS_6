de8e4498d51d05d10598b6df34653652b4769fca
Paul MIhailov <paulmihailov2013@gmail.com>
2019-03-18 22:05:23 +0300

    Add files via upload

--- custom.h
+++ custom.h
@@ -21,13 +21,21 @@ class EarthSolarRotation : public TModel
 //Earth rotation around Solar, rough calculation
 {
 protected:
-    long double mu = 132712.43994e+6; //km^3/c^2
-    long double ro = 1.0;
-    Date start, finish, checkDay;
+    static const long double startConditions[5][6];
+    const long double mu = 132712.43994e+6, //km^3/c^2
+                      J2000 = 2451544.5,   //Julian days
+                      omega = 7.292115E-5, //rad/c
+                      Re = 6371.3; //km
+    long double gnomonHeight = 0.0, ro = 1.0, S = 0.0, Sg0 = 0.0, Tstar = 0.0; //Gnomon height, Current Star Time Place calculation
+    long double longtitude = 0.0, latitude = 0.0; //Gnomon position
+    TVector Rsh, Rg, r0, ReStar, Re0;//shadow, gnomon, from Earth center to Earth surface, shadow from top of gnomon to surface, from Solar to Earth normalized
+    Date checkDay, startDates[5]; //start Dates
+    //Date start, finish, ephemerids;
 public:
     EarthSolarRotation();
     EarthSolarRotation( double t0, double tk, TVector& V );
-    EarthSolarRotation( Date d0, Date dk, Date d, TVector& V);
-    double toJulianDate(Date date);
+    EarthSolarRotation( Date dk );//, int date );
+    double toJulianDate( Date date );
+    TMatrix& getMatrixA(long double phi, long double s);
     void getRight( const TVector& X, long double t, TVector& Y );
 };
